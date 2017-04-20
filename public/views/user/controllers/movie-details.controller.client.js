769a7d27ab5f10edb4779643cdc3ba7f68fa8341
Chris O'Brien <cobrien704@gmail.com>
2017-04-20 01:26:52 -0400

    linked to movie details page

--- public/views/user/controllers/movie-details.controller.client.js
+++ public/views/user/controllers/movie-details.controller.client.js
@@ -9,6 +9,18 @@
         vm.movieId = $routeParams['mid'];
 
         function init() {
+            UserService
+                .findUserById(vm.userId)
+                .then(function(response) {
+                    var user = response.data;
+
+                    if (user) {
+                        vm.user = user
+                    } else {
+                        vm.user = 'ERROR';
+                        $location.url('/');
+                    }
+                });
 
             MovieService
                 .lookupMovieById(vm.movieId)
