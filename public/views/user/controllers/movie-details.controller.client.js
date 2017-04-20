355338a7e33557bd96de578b4fd5124154b1c364
Jess Moran <jessica.moran@gmail.com>
2017-04-20 00:25:19 -0400

    movie details pages

--- public/views/user/controllers/movie-details.controller.client.js
+++ public/views/user/controllers/movie-details.controller.client.js
@@ -3,7 +3,20 @@
         .module('Mooviews')
         .controller('MovieDetailsController', MovieDetailsController);
 
-    function MovieDetailsController($location, $scope, $routeParams) {
+    function MovieDetailsController($location, $scope, $routeParams, UserService, MovieService) {
         var vm = this;
+        vm.userId = $routeParams['uid'];
+        vm.movieId = $routeParams['mid'];
+
+        function init() {
+
+            MovieService
+                .lookupMovieById(vm.movieId)
+                .success(function (data) {
+                    vm.movie = data;
+                });
+        }
+        init();
+
     }
 })();
