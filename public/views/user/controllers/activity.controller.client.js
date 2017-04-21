22e103a2abeae56278f2bc21b62874d09683f8e5
Chris O'Brien <cobrien704@gmail.com>
2017-04-20 22:35:02 -0400

    another try….

--- public/views/user/controllers/activity.controller.client.js
+++ public/views/user/controllers/activity.controller.client.js
@@ -82,8 +82,8 @@
                                             }
                                         });
                                 });
-                            return false;
                         });
+                        return false;
                     });
             });
         }
